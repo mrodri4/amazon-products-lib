87861320a22ec7be33f19c85cb922b6a3136503e
Manuel Rodriguez <mrodriguez@avantio.com>
2023-01-15 09:26:57 +0100

    chore(Vite): Config build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -6,5 +7,18 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     preserveSymlinks: true
+  },
+  define: {
+    'process.env': {}
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'AmazonProductsLib',
+      fileName: 'bundle',
+      formats: ['iife']
+    },
+    sourcemap: false,
+    outDir: './dist'
   }
 });
