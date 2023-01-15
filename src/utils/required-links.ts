23c0cd87f313afee52a88f07afc648685df21a8e
Manuel Rodriguez <mrodriguez@avantio.com>
2023-01-15 09:59:38 +0100

    chore(requiredLinks): Change link

--- src/utils/required-links.ts
+++ src/utils/required-links.ts
@@ -1,6 +1,6 @@
 export function getLibRequiredLinks(): HTMLLinkElement[] {
   return Object.entries({
-    styles: 'http://127.0.0.1:8080/style.css'
+    styles: 'https://github.com/mrodri4/amazon-products-lib/blob/main/dist/style.css'
   }).reduce<HTMLLinkElement[]>((prev, [name, url]) => {
     const alias = `apl-${name}`;
     const link = document.createElement('link');
