39a3072cebacc464595db0b2c2c914f436247dcf
Manuel Rodriguez <mrodriguez@avantio.com>
2023-01-15 09:30:41 +0100

    chore(LibLinks): Manage lib links that must be added to shadow root and document

---
+++ src/utils/required-links.ts
@@ -0,0 +1,16 @@
+export function getLibRequiredLinks() {
+  return Object.entries({
+    styles: 'http://127.0.0.1:8080/style.css'
+  }).reduce<HTMLLinkElement[]>((prev, [name, url]) => {
+    const alias = `apl-${name}`;
+    const link = document.createElement('link');
+
+    link.setAttribute('alias', alias);
+    link.href = url;
+    link.rel = 'stylesheet';
+
+    prev.push(link);
+
+    return prev;
+  }, []);
+}
